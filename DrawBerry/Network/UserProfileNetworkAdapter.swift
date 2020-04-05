b148724cc35d3ae6d3964be1fe6a53a1f4d9e17e
Calvin <cxingzhu@hotmail.com>
2020-04-05 12:04:54 +0800

    refactor code

--- DrawBerry/Network/UserProfileNetworkAdapter.swift
+++ DrawBerry/Network/UserProfileNetworkAdapter.swift
@@ -12,9 +12,9 @@ import FirebaseStorage
 class UserProfileNetworkAdapter {
     static let db = Database.database().reference()
     static let cloud = Storage.storage().reference()
-    static var delegate: UserProfileNetworkDelegate?
     static var storageListResult: StorageListResult?
 
+    /// Uploads the profile image of a player
     static func uploadProfileImage(_ image: UIImage) {
         guard let imageData = image.jpegData(compressionQuality: 0.3),
             let userID = NetworkHelper.getLoggedInUserID() else {
@@ -22,17 +22,11 @@ class UserProfileNetworkAdapter {
         }
 
         let cloudPathRef = cloud.child("users").child(userID).child(Constants.profilePictureFileName)
-
-        cloudPathRef.putData(imageData, metadata: nil, completion: { _, error in
-            if let error = error {
-                print("Error \(error) occured while uploading to CloudStorage")
-                return
-            }
-
-        })
+        uploadDataToDatabase(data: imageData, reference: cloudPathRef)
     }
 
     // TODO: add function after game ends
+    /// Uploads the image in the to the player's gallery
     static func uploadImageToFavourites(_ image: UIImage) {
         guard let imageData = image.jpegData(compressionQuality: 0.3),
             let userID = NetworkHelper.getLoggedInUserID() else {
@@ -41,43 +35,26 @@ class UserProfileNetworkAdapter {
 
         let fileExtension = ".jpeg"
         let fileName = StringHelper.getCurrentDateTime()
-
         let cloudPathRef = cloud.child("users").child(userID).child("gallery").child(fileName + fileExtension)
 
-        cloudPathRef.putData(imageData, metadata: nil, completion: { _, error in
-            if let error = error {
-                print("Error \(error) occured while uploading to CloudStorage")
-                return
-            }
+        uploadDataToDatabase(data: imageData, reference: cloudPathRef)
 
-        })
     }
 
+    /// Loads the profile image in the `UserProfileNetworkDelegate`
     static func downloadProfileImage(delegate: UserProfileNetworkDelegate, playerUID: String?) {
         guard let playerUID = playerUID else {
             return
         }
 
         let cloudPathRef = cloud.child("users").child(playerUID).child(Constants.profilePictureFileName)
-
-        cloudPathRef.getData(maxSize: 1 * 1_024 * 1_024, completion: { data, error in
-            if let error = error {
-                print("Error \(error) occured while downloading player profile")
-                delegate.loadImage(image: nil)
-                return
-            }
-
-            guard let data = data, let image = UIImage(data: data) else {
-                print("Error occured while converting player profile")
-                delegate.loadImage(image: nil)
-                return
-            }
-
-            delegate.loadImage(image: image)
-        })
+        downloadDataFromDatabase(reference: cloudPathRef, delegate: delegate)
     }
 
+    /// Loads all images of a player's gallery in the `UserProfileViewController`
     static func getListOfImages(delegate: UserProfileViewController, playerUID: String) {
+        storageListResult = nil
+
         cloud.child("users").child(playerUID).child("gallery").listAll(completion: { result, error in
             if let error = error {
                 print("Error \(error) occured while downloading player profile")
@@ -90,6 +67,7 @@ class UserProfileNetworkAdapter {
         })
     }
 
+    /// Loads the drawing image in the `UserProfileNetworkDelegate`
     static func downloadImage(delegate: UserProfileNetworkDelegate, index: Int) {
 
         guard let result = storageListResult else {
@@ -100,16 +78,32 @@ class UserProfileNetworkAdapter {
             return
         }
 
-        let item = result.items[index]
+        let cloudPathRef = result.items[index]
+        downloadDataFromDatabase(reference: cloudPathRef, delegate: delegate)
+    }
 
-        item.getData(maxSize: 1 * 1_024 * 1_024, completion: { data, error in
+    /// Uploads image data to database
+    private static func uploadDataToDatabase(data: Data, reference: StorageReference) {
+        reference.putData(data, metadata: nil, completion: { _, error in
             if let error = error {
-                print("Error \(error) occured while downloading image")
+                print("Error \(error) occured while uploading to CloudStorage")
+                return
+            }
+        })
+    }
+
+    /// Downloads image data from database
+    private static func downloadDataFromDatabase(reference: StorageReference,
+                                                 delegate: UserProfileNetworkDelegate) {
+        reference.getData(maxSize: 1 * 1_024 * 1_024, completion: { data, error in
+            if let error = error {
+                print("Error \(error) occured while downloading data")
                 delegate.loadImage(image: nil)
                 return
             }
 
             guard let data = data, let image = UIImage(data: data) else {
+                print("Error occured during data conversion")
                 delegate.loadImage(image: nil)
                 return
             }
